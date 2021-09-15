b4602767e569a5d572099ee5e442dc10ade20e4c
Michael <mspellecacy@frakle.com>
2021-09-15 18:11:09 -0400

    Added a help, reorgd some code.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 require('dotenv').config(); //initialize dotenv
 const Discord = require("discord.js");
 const { Intents } = require("discord.js");
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const CLIENT_TOKEN = process.env.CLIENT_TOKEN;
 const MAP_API_KEY = process.env.MAP_API_KEY;
 const MAP_TRIGGER = '!getmap';
+const MAP_HELP_TRIGGER = 'help';
 const MAP_BASE = 'https://maps.googleapis.com/maps/api/staticmap?'
-const BADLOC_MSG = 'Bad Location specified, expected format \'47.04,-122.86\'';
+const BADLOC_MSG = 'Bad Location specified, expected format \'47.04 -122.86\'';
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 function getMap(params) {
-    // Start with our base url.
-    let mapUrl = MAP_BASE;
-
     console.debug('Map Params: ', params);
+    if(!(params[1] && params[2])) return BADLOC_MSG;
 
     // See: https://developers.google.com/maps/documentation/maps-static/start
     const mapParameters = {
-        center: `${params[1]}${params[2]}` || '',
+        center: `${params[1]},${params[2]}` || '',
         zoom: params[3] || '15',  // Default to 15, street/building level.
         size: params[4] || '500x500',
         scale: '', // TODO: Implement scale optional parameter.
@@ -24,8 +24,11 @@ function getMap(params) {
         maptype: 'hybrid',  // Hardcoding to Hybrid for the time being
     }
 
-    // Basic sanity test to exit early
-    if(mapParameters.center.split(',').length !== 2) return BADLOC_MSG;
+    console.debug('Parsed Params: ', mapParameters);
+
+    // Construct our url
+    // Start with our base url.
+    let mapUrl = MAP_BASE;
 
     // Center is required (its where we're focusing)
     mapUrl += `center=${mapParameters.center}`;
@@ -44,17 +47,27 @@ function getMap(params) {
 
     // Return our url.
     return mapUrl;
+}
 
+function getMapHelp() {
+    const EXAMPLE_TEXT = '```!getmap 36.01611081769955, -114.73728899363789 18 200x200```\n' +
+        '**Example Key: **!getmap {lat} {lon} {zoom} {size}\n\n' +
+        'Only Lat and Lon are required, commas may be omitted.\n ' +
+        'Zoom Level (default: 15) and Image Size (default 500x500) are Optional.';
+    return EXAMPLE_TEXT;
 }
 
 function handleMessage(msg) {
     if(msg.content.startsWith(MAP_TRIGGER)) {
-        const params = msg.content.split(' ');
-        const map = getMap(params);
+        const params = msg.content.replace(',', '').split(' ');
 
-        console.debug('Returning Map Url: ', map);
-
-        msg.channel.send(map);
+        if(params.length < 3 || params[1] === MAP_HELP_TRIGGER) {
+            msg.channel.send(getMapHelp());
+        }  else {
+            const map = getMap(params);
+            console.debug('Returning Map Url: ', map);
+            msg.channel.send(map);
+        }
     }
 }
 
