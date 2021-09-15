9c72fd726261dfb5307d92d0abcb101fcf58590d
Michael <mspellecacy@frakle.com>
2021-09-15 13:13:14 -0400

    Cleanup, minor tweaks.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ require('dotenv').config(); //initialize dotenv
 const Discord = require("discord.js");
 const { Intents } = require("discord.js");
 const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const CLIENT_TOKEN = process.env.CLIENT_TOKEN
+const CLIENT_TOKEN = process.env.CLIENT_TOKEN;
+const MAP_API_KEY = process.env.MAP_API_KEY;
 const MAP_TRIGGER = '!getmap';
 const MAP_BASE = 'https://maps.googleapis.com/maps/api/staticmap?'
 const BADLOC_MSG = 'Bad Location specified, expected format \'47.04,-122.86\'';
@@ -19,7 +20,7 @@ function getMap(params) {
         zoom: params[3] || '15',  // Default to 15, street/building level.
         size: params[4] || '500x500',
         scale: '', // TODO: Implement scale optional parameter.
-        format: '', // TODO: Implement format option parameter.
+        format: '', // TODO: Implement format optional  parameter.
         maptype: 'hybrid',  // Hardcoding to Hybrid for the time being
     }
 
@@ -29,17 +30,17 @@ function getMap(params) {
     // Center is required (its where we're focusing)
     mapUrl += `center=${mapParameters.center}`;
 
-    // Zoom is optional, but the api is worthless if you dont set it.
+    // Maptype is optional but defaults to roads only, we want hybrid for now.
     mapUrl += `&maptype=${mapParameters.maptype}`;
 
-    // Size is a required parameter.
+    // Zoom is optional, but the api is worthless if you dont set it.
     mapUrl += `&zoom=${mapParameters.zoom}`;
 
     // Size is a required parameter.
     mapUrl += `&size=${mapParameters.size}`;
 
     // Attach our API Key
-    mapUrl += `&key=${process.env.MAP_API_KEY}`;
+    mapUrl += `&key=${MAP_API_KEY}`;
 
     // Return our url.
     return mapUrl;
